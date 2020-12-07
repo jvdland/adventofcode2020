ab0ff86b69b3dd3870597d04b9feca0a42bb9209
Joost van der Land <vanderland@evident.nl>
2020-12-07 21:37:06 +0000

    this does not hurt my eyes

--- day7/puzzle1.py
+++ day7/puzzle1.py
@@ -1,42 +1,27 @@
 import os, re
-child_with_parents = {}
 def read_rules():
-    #fn = os.path.join(os.path.dirname(__file__), 'sample1.input')
     fn = os.path.join(os.path.dirname(__file__), 'puzzle1.input')
-    rules = []
+    #fn = os.path.join(os.path.dirname(__file__), 'sample1.input')
+    rules = {}
     with open(fn) as f:
         for line in f:
-            rule = line.strip().split("contain")
-            left = [rule[0].replace("bags", "").strip()]
-            right = rule[1].strip().split(',')
-            right = [w.replace('bags', '').replace('bag', '').replace('.', '').strip() for w in right]
-            right = [re.sub('\d', '', w).strip() for w in right]
-            left.extend(right)
-            rules.append(left)
+            pair = line.split("contain")
+            parent = pair[0].replace("bags", "").strip()
+            children = [re.sub(" \d+", "", w) for w in pair[1].split(",")]
+            children = [w.replace(".", "").replace("bags", "").replace("bag","").strip() for w in children]
+            rules[parent] = children
+
     return rules
 
-def add_parents(rule, child_with_parents) :
-    parent = rule.pop(0)
-    for child in rule :
-        if not child in child_with_parents.keys():
-            child_with_parents[child] = set()
-        child_with_parents[child].add(parent)
-    return child_with_parents
-
-def add_grand_parents(child_with_parents):
-    for child, parents in child_with_parents.items():
-        for parent in parents :
-            if parent in child_with_parents.keys():
-                child_with_parents[child] = child_with_parents[child].union(child_with_parents[parent])
-    return child_with_parents
+def find_parents_for_key(key, allready_found, rules):
+    res = []
+    for parent, children in rules.items():
+        if key in children and parent not in allready_found:
+            res.append(parent)
+            allready_found.append(parent)
+            res.extend(find_parents_for_key(parent, allready_found, rules))
+    return res
 
 rules = read_rules()
-for rule in rules:
-    child_with_parents = add_parents(rule, child_with_parents)
-
-child_with_parents = add_grand_parents(child_with_parents)
-child_with_parents = add_grand_parents(child_with_parents)
-child_with_parents = add_grand_parents(child_with_parents)
-
-
-print(len(child_with_parents['shiny gold']))
+res = find_parents_for_key('shiny gold', [], rules)
+print(len(res))
